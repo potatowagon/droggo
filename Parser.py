eb07ab8b32cb12c634046d7e6ab4425199c5b183
ADS\t_wongsh <sherry.wong.wen.qi@autodesk.com>
2018-05-09 17:03:18 +0800

    parameter mapper fills up based on argv

--- Parser.py
+++ Parser.py
@@ -3,9 +3,12 @@ import os  # os.sep
 import sys  # sys.exit
 import stat
 import time
-import traceback
 import datetime
-import getopt
+import traceback
+
+import Logging
+
+log = Logging.Log()
 
 class ParameterMapper():
     params = {}
@@ -13,7 +16,8 @@ class ParameterMapper():
         'file_path' : ['--fetch'],
         'org' : ['--from'],
         'find' : ['--find'],        
-        'replace' : ['--replace', '--rep']
+        'replace' : ['--replace', '--rep'],
+        'credentials' : ['-u']
     }
     argv = None
 
@@ -21,4 +25,24 @@ class ParameterMapper():
         self.argv = argv
         self.fill()
 
-    def fill(self)
+    def fill(self):
+        for key in self.keywords:
+            missing_count = 0
+            for word in self.keywords[key]:
+                try:
+                    index = self.argv.index(word)
+                    break
+                except:
+                    missing_count += 1
+                    #searched through the whole list of keywords but unable to find keyword in argv
+                    if missing_count == len(self.keywords[key]):
+                        log.err("Missing Argument for " + key)
+                        sys.exit(1)
+
+            if key in self.params:
+                log.err("Duplicate parameter for " + key)
+                sys.exit(1)
+            else:
+                self.params[key] = self.argv[index+1]
+
+                
