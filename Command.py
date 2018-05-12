dab888df6d133a0e0d2b65b3bf69b53e06343071
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-05-12 17:13:35 +0800

    makeapi calls to get org, get repos

--- Command.py
+++ Command.py
@@ -1,3 +1,5 @@
+import os  # os.sep
+import sys  # sys.exit
 import Credentials
 import Logging
 
@@ -21,5 +23,33 @@ class Command():
     def init_remote(self):
         try:
             self.github = Github(base_url="https://{self.credentials.username}/api/v3", login_or_token=self.credentials.password)
-        except:
-            log
+        except Exception as e:
+            print(e)
+            log.info("Log in as enterprise failed. Now trying log in as personal github")
+            try:
+                self.github = Github(self.credentials.username, self.credentials.password)
+            except Exception as e:
+                print(e)
+                log.err("Login failed")
+                sys.exit(1)
+
+        try:
+            self.org = self.github.get_organization(params.org)
+        except Exception as e:
+            log.err(e)
+
+    def get_repos(self):
+        try:
+            self.repos = self.org.get_repos()
+        except Exception as e:
+            print(e)
+
+    def execute(self):
+        self.init_remote()
+        self.get_repos()
+        for repo in self.repos:
+            # self.clone()
+            # self.find_and_replace()  
+            pass  
+        
+        
