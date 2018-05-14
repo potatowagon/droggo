857febe71b6644fada5c05c77f6b6ab1305cdbb4
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-05-14 18:37:06 +0800

    disable logging for comand

--- Command.py
+++ Command.py
@@ -1,12 +1,14 @@
 import os  # os.sep
 import sys  # sys.exit
+import traceback
+
 import Credentials
 import Logging
 
 from github import Github
 
 
-log = Logging.Log
+#log = Logging.Log
 
 class Command():
 
@@ -17,30 +19,37 @@ class Command():
     github = None
 
     def __init__(self, params):
+        print("here")
         self.params = params
-        self.credentials = Credentials.Credentials(params.credentials)
+        self.credentials = Credentials.Credentials(params["credentials"])
 
     def init_remote(self):
         try:
+            print(self.credentials.username, self.credentials.password)
             self.github = Github(base_url="https://{self.credentials.username}/api/v3", login_or_token=self.credentials.password)
+            print(self.github)
         except Exception as e:
             print(e)
-            log.info("Log in as enterprise failed. Now trying log in as personal github")
+            print("Log in as enterprise failed. Now trying log in as personal github")
+            #log.info("Log in as enterprise failed. Now trying log in as personal github")
             try:
                 self.github = Github(self.credentials.username, self.credentials.password)
             except Exception as e:
                 print(e)
-                log.err("Login failed")
+                print("Login failed")
+                #log.err("Login failed")
                 sys.exit(1)
 
         try:
-            self.org = self.github.get_organization(params.org)
+            pass
         except Exception as e:
-            log.err(e)
+            print(e)
+            
 
     def get_repos(self):
         try:
-            self.repos = self.org.get_repos()
+            #self.repos = self.org.get_repos()
+            self.repos = self.github.get_repos()
         except Exception as e:
             print(e)
 
@@ -49,7 +58,8 @@ class Command():
         self.get_repos()
         for repo in self.repos:
             # self.clone()
-            # self.find_and_replace()  
-            pass  
+            # self.find_and_replace()
+            # self.PR()  
+            print(repo.name)
         
         
