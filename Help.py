87b1e9fc71762f2eeb0a2d2986a75cb07fde33d8
ADS\t_wongsh <sherry.wong.wen.qi@autodesk.com>
2018-05-21 19:06:08 +0800

    wip

--- Help.py
+++ Help.py
@@ -1,4 +1,8 @@
-string = "This is the help page"
+import Parser 
 
 def show():
     print(string)
+
+def format_instructions(keyword_dict, keyword_dict_name):
+    out = keyword_dict_name + ":\n"
+    #for key
