c2fc42a1f6c1410f418701bdd96e9acf76981496
ADS\t_wongsh <sherry.wong.wen.qi@autodesk.com>
2018-05-21 15:02:40 +0800

    escape @ in password

--- Command.py
+++ Command.py
@@ -147,9 +147,9 @@ class Command():
 
     def push(self):
         remote = self.github_clone_url[:8] + self.credentials.username + \
-            ":" + self.credentials.password + "@" + self.github_clone_url[8:]
+            ":" + self.credentials.password.replace("@", "%40") + "@" + self.github_clone_url[8:]
         git = self.cloned_repo.git
-        git.push(remote, self.new_branch_name +
+        git.push(remote, "--force", self.new_branch_name +
                  ":" + self.new_branch_name)
         print("Pushed to remote")
 
